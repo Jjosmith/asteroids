f0561e3c2e1477c28faf82953c6a16f9a42e8ced
Jack Smith <jack.smith@landregistry.gov.uk>
2026-01-30 09:57:23 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+from player import Player
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
